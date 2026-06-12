aede390bed9226266c5bf32f217738dcaa9e1e6a
Magnus <a.marcellin@reply.com>
2026-06-12 13:28:05 +0200

    Initial Commit

---
+++ ClaudeVault/ClaudeVaultApp.swift
@@ -0,0 +1,32 @@
+//
+//  ClaudeVaultApp.swift
+//  ClaudeVault
+//
+//  Created by Alois Marcellin on 12/06/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ClaudeVaultApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
